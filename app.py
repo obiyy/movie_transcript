2dde1f88fee6f732a87c3b1293d2092b65b6491a
obiyy <obino@m.livemania.net>
2025-07-01 14:18:48 +0000

    first commit

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def get_subtitles():
         return jsonify(result)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-s
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
